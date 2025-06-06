7e76b5861281a1a6450dbac8a2066d0da733087f
binthon <74725795+binthon@users.noreply.github.com>
2025-06-06 21:30:08 +0200

    Create check_ip.py

---
+++ check_ip.py
@@ -0,0 +1,20 @@
+from ipaddress import ip_address
+
+class IPAllocator:
+    def __init(self, start_ip='100.20.10.2'):
+        self.base_ip = ip_addresess(start_ip)
+        self.allocated = {}
+        self.last_ip = self.base_ip
+    def assign(self, addr):
+        if addr in self.allocated:
+            return self.allocated(addr)
+        self.last_ip += 1
+        self.allocated[addr] = str(self.last_ip)
+        return self.allocated[addr]
+
+    def get_all_assignments(self):
+        return self.allocated.copy()
+
+    def release_ip(self, addr):
+        if addr in self.allocated:
+            del self.allocated[addr] 
