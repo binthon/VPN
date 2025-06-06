ddb9af0741997527653daa4ea53e8eefe3644d41
binthon <74725795+binthon@users.noreply.github.com>
2025-06-06 21:29:32 +0200

    Update aes.py

--- aes.py
+++ aes.py
@@ -2,26 +2,16 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
 
-KEY = None 
-
-def set_key(new_key: bytes):
-    global KEY
-    KEY = new_key
-
-def encrypt(data):
-    if KEY is None:
-        raise ValueError("AES KEY is not set.")
+def encrypt(data: bytes, key: bytes) -> bytes:
     iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(KEY), modes.CFB(iv), backend=default_backend())
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     encryptor = cipher.encryptor()
     ct = encryptor.update(data) + encryptor.finalize()
     return iv + ct
 
-def decrypt(encrypted_data):
-    if KEY is None:
-        raise ValueError("AES KEY is not set.")
+def decrypt(encrypted_data: bytes, key: bytes) -> bytes:
     iv = encrypted_data[:16]
     ct = encrypted_data[16:]
-    cipher = Cipher(algorithms.AES(KEY), modes.CFB(iv), backend=default_backend())
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
     decryptor = cipher.decryptor()
     return decryptor.update(ct) + decryptor.finalize()
